eba7d8714f2853c1bd59b77f713335b92937c8e2
emmanuelfalola <emmanuelfalola9998u@gmail.com>
2024-04-10 11:45:39 +0100

    added logic for how random squares will be chosen

--- whac-a-mole/src/main.ts
+++ whac-a-mole/src/main.ts
@@ -1,1 +1,30 @@
-import './main.scss'
+import './main.scss';
+
+const cells = document.querySelectorAll(".board__cell") as NodeListOf<HTMLElement>;
+const mole = document.querySelector(".mole") as HTMLElement | null;
+const timeRemaining = document.querySelector(".time-remaining") as HTMLElement;
+const score = document.querySelector(".score-runner") as HTMLElement;
+
+if (!cells || !timeRemaining || !score) {
+    throw new Error("Required elements not found.");
+}
+
+let result = 0;
+
+const randomCell = () => {
+  cells.forEach((cell) => {
+      cell.classList.remove("mole");
+  });
+
+  let randomIndex = Math.floor(Math.random() * cells.length);
+  let randomPosition = cells[randomIndex];
+  randomPosition.classList.add("mole");
+}
+
+randomCell();
+
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", () => {
+  });
+});
